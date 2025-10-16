ea6b4f97b05145ca70952a6308dfa8a9ee2da95b
Christian Verzosa <christian.eclevia.verzosa@gmail.com>
2025-10-16 12:38:43 +0800

    feature: Login page components UI only

--- features/src/lib/login/components/login-page-content.tsx
+++ features/src/lib/login/components/login-page-content.tsx
@@ -1,3 +1,15 @@
+import { LoginForm } from './login-form';
+
 export const LoginPageContent = () => {
-  return <p>this is the login page content</p>;
+  return (
+    <div className="flex flex-col space-y-5 items-center md:border p-10 md:shadow-xl w-full max-w-sm">
+      <div className="flex items-center flex-col space-y-2">
+        {/**  Brand Logo */}
+        <div className="h-20 w-20 bg-gray-100" />
+        {/** Title */}
+        <h1 className="text-2xl font-bold">Megaton Portal</h1>
+      </div>
+      <LoginForm />
+    </div>
+  );
 };
