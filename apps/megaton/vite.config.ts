c97b62dd4f7afc557dbf60c2f6c3720bc2d13ec8
Christian Verzosa <christian.eclevia.verzosa@gmail.com>
2025-10-16 22:57:50 +0800

    fix: Temporary add base in megaton vite config for gh pages

--- apps/megaton/vite.config.ts
+++ apps/megaton/vite.config.ts
@@ -6,6 +6,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/megaton',
+  base: '/apollo/megaton/',
   server: {
     port: 3000,
     host: 'localhost',
