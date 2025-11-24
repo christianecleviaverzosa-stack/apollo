4da044717bc4ca9d90e02c7442fc1e076c240cc3
Christian Verzosa <christian.eclevia.verzosa@gmail.com>
2025-11-24 19:32:20 +0800

    fix: Add environment file on remote repository

---
+++ apps/megaton/public/env.js
@@ -0,0 +1,11 @@
+var environment = {
+  production: false,
+  brand: 'Megaton',
+  brandCode: 'MEGATON',
+  service: {
+    web: {
+      baseUrl: 'latest.tradehousecrm.com',
+    },
+  },
+  features: [],
+};
